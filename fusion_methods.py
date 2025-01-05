bug fixes for sumnorm and minmax normalization fusion methods

fusion_methods.py
@@ -145,7 +145,8 @@ def weighted_min_max(trec_runs, weights, max_docs=1000):
             for docid in docs_for_run:
                 if max_weight != min_weight:
                     doc_scores[docid] = doc_scores.get(docid, 0.0) + (weight - min_weight) / (max_weight - min_weight)
-
+                else:
+                    doc_scores[docid] = doc_scores.get(docid, 0.0)
 
         # Sort by score (descending) and then by docid (ascending) for consistent tie-breaking
         sorted_docs = sorted(doc_scores.items(), key=lambda x: (-x[1], x[0]))[:max_docs]
@@ -177,7 +178,7 @@ def weighted_sumnorm(trec_runs, weights, max_docs=1000):
             run_docs[r] = docs
             all_docs.update(docs)
 
-        top100_sum = np.nansum(np.sort(weights, axis=1)[:, -100:], axis=1)
+        top100_sum = np.nansum(np.sort(weights)[-100:])
         # Calculate scores using stored documents
         for r, weight in zip(trec_runs, weights):
             docs_for_run = run_docs[r]
@@ -186,6 +187,8 @@ def weighted_sumnorm(trec_runs, weights, max_docs=1000):
             for docid in docs_for_run:
                 if top100_sum!=0:
                     doc_scores[docid] = doc_scores.get(docid, 0.0) + weight/top100_sum
+                else:
+                    doc_scores[docid] = doc_scores.get(docid, 0.0)
 
         # Sort by score (descending) and then by docid (ascending) for consistent tie-breaking
         sorted_docs = sorted(doc_scores.items(), key=lambda x: (-x[1], x[0]))[:max_docs]